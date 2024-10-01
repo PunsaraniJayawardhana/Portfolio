30d118c120f5034c0e6d623e4c8a2904baa22d3c
PunsaraniJayawardhana <punsaranijayawardhana@gmail.com>
2024-10-01 15:43:16 +0530

    new

--- components/Home/Contact/ContactInfo.tsx
+++ components/Home/Contact/ContactInfo.tsx
@@ -4,33 +4,29 @@ import { FaEnvelope, FaPhone } from 'react-icons/fa'
 
 const ContactInfo = () => {
   return (
-    <div 
-    data-aos="fade-right">
-        <div className="flex items-center space-x-8">
-           <div className="w-10 h-10 md:w-16 md:h-16 rounded-full bg-gradient-to-r from-emerald-500 to-emerald-900
-           flex items-center justify-center flex-col">
-            <FaPhone className="w-4 h-4 md:h-7 text-white" />
-            </div>
-            <div>
-                <h1 className="text-lg sm:text-xl text-white font-bold">Phone</h1>
-                <h1 className="text-base sm:text-lg text-white text-opacity-70">
-                    {contactData.phone}
-                </h1>
-            </div>  
+    <div data-aos="fade-right">
+      <div className="flex items-center space-x-8">
+        <div className="w-10 h-10 md:w-16 md:h-16 rounded-full bg-gradient-to-r from-emerald-500 to-emerald-900 flex items-center justify-center flex-col">
+          <FaPhone className="w-6 h-6 md:h-8 md:w-8 text-white" /> {/* Adjusted size */}
         </div>
-        <div className="flex items-center space-x-8 mt-8 mb-8">
-           <div className="w-10 h-10 md:w-16 md:h-16 rounded-full bg-gradient-to-r from-emerald-500 to-emerald-900
-           flex items-center justify-center flex-col">
-            <FaEnvelope className="w-4 h-4 md:h-7 text-white" />
-            </div>
-            <div>
-                <h1 className="text-lg sm:text-xl text-white font-bold">Email Address</h1>
-                <h1 className="text-base sm:text-lg text-white text-opacity-70">
-                    {contactData.email}
-                </h1>
-            </div>  
+        <div>
+          <h1 className="text-lg sm:text-xl text-white font-bold">Phone</h1>
+          <h1 className="text-base sm:text-lg text-white text-opacity-70">
+            {contactData.phone}
+          </h1>
         </div>
-        
+      </div>
+      <div className="flex items-center space-x-8 mt-8 mb-8">
+        <div className="w-10 h-10 md:w-16 md:h-16 rounded-full bg-gradient-to-r from-emerald-500 to-emerald-900 flex items-center justify-center flex-col">
+          <FaEnvelope className="w-6 h-6 md:h-8 md:w-8 text-white" /> {/* Adjusted size */}
+        </div>
+        <div>
+          <h1 className="text-lg sm:text-xl text-white font-bold">Email Address</h1>
+          <h1 className="text-base sm:text-lg text-white text-opacity-70">
+            {contactData.email}
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
